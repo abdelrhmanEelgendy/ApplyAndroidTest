a17a660932e15adabdc9c28453887bf531a0f863
abdelrhmanelgeny <abdelrhmanelgendy243@gmail.com>
2023-08-24 16:43:51 +0300

    first commit

---
+++ app/src/androidTest/java/com/example/applycicd/ExampleInstrumentedTest.kt
@@ -0,0 +1,54 @@
+package com.example.applycicd
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.applycicd", appContext.packageName)
+    }
+    @Test
+    fun useAppContext0() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.applycicd", appContext.packageName)
+    }
+    @Test
+    fun useAppContext1() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.applycicd", appContext.packageName)
+    }
+    @Test
+    fun useAppContext2() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.applycicd", appContext.packageName)
+    }
+//    @Test
+//    fun useAppContext3() {
+//        // Context of the app under test.
+//        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+//        assertEquals("com.example.applycicd", "-1")
+//    }
+    @Test
+    fun useAppContext4() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.applycicd", appContext.packageName)
+    }
+}
